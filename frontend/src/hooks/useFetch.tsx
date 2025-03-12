bc671f999334abe3e6613a9012d092c297477329
alex-bo7 <anino882@gmail.com>
2025-03-12 15:58:36 -0700

    Set up json server + useFetch component

---
+++ frontend/src/hooks/useFetch.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const responce = await fetch(url);
+        if (!responce.ok) throw new Error(responce.statusText);
+
+        const result = await responce.json();
+        setData(result);
+        setLoading(false);
+        setError("");
+      } catch (error) {
+        setError(error instanceof Error ? error.message : "An error occured");
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
